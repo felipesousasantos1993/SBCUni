d3730c74cfb772095c7267abe057d6dc2d07f1e3
felipesousasantos1993@gmail.com <felipesousasantos1993@gmail.com@4f997932-ca79-fb40-8dcd-fe38f7574b00>
2014-09-18 20:56:02 +0000

    novo padrão visual

---
+++ branches/NovoTema/web/src/main/webapp/resources/js/marca-dagua.js
@@ -0,0 +1,8 @@
+// mask
+$(document).ready(function() {
+	marcaDagua();
+});
+function marcaDagua() {
+	$("#matricula").attr("placeholder", "Matrícula").blur();
+	$("#senha").attr("placeholder", "Senha").blur();
+}
